4e82b5973f188e8848b0f9241b02d59a0d31c6cc
iamkoks <iamkoks@users.noreply.github.com>
2020-07-01 16:20:40 +0300

    component BookAdd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Books from './components/Books'
+import BookAdd from './components/BookAdd'
 
 function App() {
   return (
     <div className="container">
-       <Books books={[1,2,3]}></Books>
+        <BookAdd></BookAdd>
+        <Books books={[1,2,3]}></Books>
     </div>
   );
 }
---
+++ src/components/BookAdd.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+
+function BookEnd(){
+
+        const[books, setBook] = useState(
+        [
+            {
+                name: "Война и Мир"
+            }
+        ]
+        )
+        
+        const[valueBook, setValueBook] = useState('');
+        const [clickAdd, setAddClick] = useState(false);
+        const [smShow, setSmShow] = useState(false);
+        const [indexDelete, setIndexDelete] = useState(-1)
+        const [indexChange, setIndexChange] = useState(-1)
+
+        useEffect(() => {
+            if (clickAdd && indexChange < 0) {
+                books.push({
+                    name: valueBook
+                });
+                setBook(books);
+                setValueBook('');
+            }
+            else if(clickAdd && indexChange >= 0){
+                books.splice(indexChange, 1, {name: valueBook})
+                setBook(books);
+                setIndexChange(-1);
+                setValueBook('');
+            }
+            setAddClick(false);
+        }, [clickAdd])
+
+        useEffect(() => {
+            if (indexDelete>=0) {
+                books.splice(indexDelete, 1)
+                setBook(books);
+                setIndexDelete(-1);
+            }
+        }, [indexDelete])
+
+        useEffect(() => {
+            if (indexChange>=0) {
+                setValueBook(books[indexChange].name)
+            }
+        }, [indexChange])
+
+        const Add = () => {
+            if(!valueBook)
+            {
+                return (
+                    setSmShow(true)
+                )
+            }
+            else
+            {
+                setAddClick(true)
+            }
+        }
+
+            
+    return(
+        <div>
+            <input value={valueBook} onChange={(e) => setValueBook(e.target.value)} style={{width: '440px', marginLeft: '30px', marginTop: '10px'}}>
+            </input>
+            <button onClick={() => Add()}>{ (indexChange>=0) ? "Изменить" : "Добавить"}</button>
+        </div>   
+    )
+}
+
+export default BookEnd;
