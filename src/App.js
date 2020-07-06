8d644152595dda5267b03d09e9161ef648e3a0d1
iamkoks <iamkoks@users.noreply.github.com>
2020-07-06 14:41:48 +0300

    more fix + footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Books from './components/Books/Books'
 import Header from './components/Header/Header';
+import Bottom from './components/Bottom/Bottom';
 import 'antd/dist/antd.css';
 
 import './App.scss'
+
 
 function App() {
   return (
     <div className="container">
         <Header></Header>
         <Books></Books>
+        <Bottom></Bottom>
     </div>
   );
 }
---
+++ src/components/Bottom/Bottom.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Bottom.scss'
+
+const Bottom = () => {
+   return (
+    <footer>
+        <div>
+            <a className="link">Библиотека, 2020</a>
+        </div>
+    </footer>
+    
+   ) 
+}
+
+export default Bottom
