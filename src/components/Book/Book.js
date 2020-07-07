b11e737214cdf5b1be75a91956a86e954495e32b
iamkoks <iamkoks@users.noreply.github.com>
2020-07-07 12:19:45 +0300

    button_for_bookList

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Card } from 'antd';
+import { Button, Tooltip } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 
 import './Book.scss'
 
@@ -12,6 +14,7 @@ const Book = (props) => {
         hoverable
         cover={<img alt="example" src={props.book.cover} />}
     >
+        <Button type="primary" shape="circle" icon={<SearchOutlined />} />
         <Meta title={props.book.name} description={"Автор: "+props.book.author} />
     </Card>
     </div>
