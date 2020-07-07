d2531393c274d01580882604ad3f4f8b2d803320
iamkoks <iamkoks@users.noreply.github.com>
2020-07-07 14:55:23 +0300

    fix+footer

--- src/components/Carousel/CarouselSlide.js
+++ src/components/Carousel/CarouselSlide.js
@@ -3,8 +3,12 @@ import React from 'react'
 import './CarouselSlide.scss'
 
 const CarouselSlide = (props) => {
+    const onChange = () =>{
+        
+    }
+
    return (
-    <div className="carousel-slide">
+    <div className="carousel-slide" onClick={() => onChange()}>
         <img src={props.slide.img} alt=""></img>
     </div>
     
