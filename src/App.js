4be24eaa3cd494404ee10c0a7df23c8eeefb4c90
iamkoks <iamkoks@users.noreply.github.com>
2020-07-09 16:27:43 +0300

    more refactoring

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import {
-    BrowserRouter as Switch,
-    Route,
-    HashRouter
-  } from "react-router-dom";
 
 import Books from './components/Books/Books'
 import Header from './components/Header/Header';
 import Bottom from './components/Bottom/Bottom';
 import BookCarousel from './components/Carousel/BookCarousel';
-import BookTitle from './components/BookTitle/BookTitle'
-import AboutBook from './components/BookAbout/BookAbout'
 import { BackTop } from 'antd';
 import 'antd/dist/antd.css';
 import './App.scss'
@@ -18,18 +11,16 @@ import './App.scss'
 
 function App() {
     return (
-       
-            <div className="container">
-                <div>
-                    <BackTop/>
-                    <Header/>
-                    <BookCarousel/>
-                    <Books/>
-                      
-                </div>
-                
-                <Bottom/>
+        <div className="container">
+            <div>
+                <BackTop/>
+                <Header/>
+                <BookCarousel/>
+                <Books/>
             </div>
+        
+            <Bottom/>
+        </div>
     );
 }
 
